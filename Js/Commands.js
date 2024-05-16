55ee75cfbb4ac8cee6f57d0c5b9e863b8034e638
dvizioon <danielmartinsjob@gmail.com>
2024-05-16 18:24:23 -0300

    add New Pages

--- Js/Commands.js
+++ Js/Commands.js
@@ -4,10 +4,12 @@ function loadContent(page) {
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             document.querySelector('.toolsview').style.display = "none"
-            document.getElementById('contentRender').style.display="block"
+            document.getElementById('contentRender').style.display = "block"
+            document.getElementById('lineChart').style.display = "none"
             document.getElementById('contentRender').innerHTML = this.responseText;
         } else {
             document.querySelector('.toolsview').style.display = "block"
+            document.getElementById('lineChart').style.display = "block"
             document.getElementById('contentRender').style.display = "none"
         }
     };
